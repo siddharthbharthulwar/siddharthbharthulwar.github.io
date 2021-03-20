0f49debc46a06d754d7c58c9b296c0a0dfe98611
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2021-03-19 21:54:31 -0600

    revert

--- src/App.js
+++ src/App.js
@@ -1,54 +1,10 @@
+import React from 'react'
 
 
 function App() {
   return (
-    <div class = "wrapper">
-      <header>
-
-
-
-          <ul class="social">
-            <li><a href="mailto:siddharth.bharthulwar@gmail.com" target="_blank"><i class="far fa-envelope"></i></a></li>
-            <li><a href="//github.com/siddharthbharthulwar/"><i class="fab fa-github"></i></a></li>
-            <li><a href="//linkedin.com/in/sbharthulwar"><i class="fab fa-linkedin-in"></i></a></li>
-
-        </ul>
-
-          <ul class = "sidebar-nav">
-            <li class = "sidebar-nav-item"><a href = "/education.html">[education]</a></li>
-            <li class="sidebar-nav-item"><a href="/projects">[research + projects]</a></li>
-            <li class = "sidebar-nav-item"><a href = "/awards">[awards + honors]</a></li>
-            <li class="sidebar-nav-item"><a href="/organizations">[organizations]</a></li>
-            <li class = "sidebar-nav-item"><a href = "assets/updated-often/resume2020.pdf">[resume]</a></li>
-          
-          </ul>
-
-        </header>
-
-        <main>
-
-          <div class = "intro">
-
-              <h4>Hey, I'm Siddharth (Sid) Bharthulwar. I'm a senior at Fairview High School in Boulder, Colorado. 
-                I've grown to see computation as a catalyst for social good, having engineered a machine-learning-powered tool to increase CT scan availability, developed a computer vision system for low-visibility airliner navigation, and modeled traffic congestion
-                in Denver's Flatiron Flyer bus routes with neural networks.</h4>
-          </div>
-          
-          <div class = "text-content-container">
-            <h2>What I'm Working on Now</h2>
-            
-            <ul>
-
-              <li>Research @ University of Pennsylvania Medical School (CT Imaging + ML)</li>
-              <li>Research + SWE @ Denver Regional Transportation District (Traffic Congestion + ML/React)</li>
-              <li>Learning Full-Stack Software Development (MERN Stack)</li>
-
-            </ul>
-          </div>
-
-
-        </main>
-
+    <div class = "container">
+      <h1>Hi</h1>
     </div>
   );
 }
