7a0e05814de7d56a8d8f460c167ba6c46acd86a4
SidBharthulwar <siddharth.bharthulwar@gmail.com>
2021-03-21 23:04:58 -0600

    add particles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from './components/pages/Main'
 import Education from './components/pages/Education'
 import Experience from './components/pages/Experience'
 import Projects from './components/pages/Projects'
+import Particles from 'react-particles-js'
 
 
 
@@ -13,7 +14,51 @@ function App() {
   var image = require('./img/Bharthulwar.Siddharth.Headshot.jpg')
 
   return (
+    <div>
       <Router>
+      <Particles style = {{zIndex: "0", position: "absolute"}} params={{
+	    "particles": {
+	        "number": {
+	            "value": 60,
+	            "density": {
+	                "enable": true,
+	                "value_area": 1500
+	            }
+	        },
+	        "line_linked": {
+	            "enable": true,
+	            "opacity": 0.02
+	        },
+	        "move": {
+	            "direction": "right",
+	            "speed": 0.05
+	        },
+	        "size": {
+	            "value": 1
+	        },
+	        "opacity": {
+	            "anim": {
+	                "enable": true,
+	                "speed": 1,
+	                "opacity_min": 0.05
+	            }
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onclick": {
+	                "enable": true,
+	                "mode": "push"
+	            }
+	        },
+	        "modes": {
+	            "push": {
+	                "particles_nb": 1
+	            }
+	        }
+	    },
+	    "retina_detect": true
+	}}/>
       <div className = "wrapper">
         <header>
           <div className = "author-name">
@@ -41,6 +86,7 @@ function App() {
         </main>
       </div>
       </Router>
+      </div>
   );
 }
 
--- src/components/School.js
+++ src/components/School.js
@@ -2,12 +2,59 @@ import React from 'react'
 
 const School = ( { school } ) => {
 
-   /* var image = require(`../Img/${school.imgFileName}`)*/
+   var image = require(`../img/${school.imgFileName}`)
+
+   const schoolStyle = {
+        height: "150px",
+        width: "1000px",
+        color: "white",
+        borderRadius: "15px",
+        boxShadow: "0 6px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+        marginLeft: "25px",
+        textAlign: "left",
+        position : "relative",
+        marginBottom: "22px",
+        backdropFilter: "blur(11px)",
+        lineHeight: "1.0",
+        backgroundColor: "#1b1b1b"
+       
+   }
+
+   const leftStyle = {
+
+        float: "left",
+        width: "20",
+        margin: "auto",
+        display: "block",
+        paddingTop: "25px",
+        paddingLeft: "25px"
+
+   }
+
+   const rightStyle = {
+
+        float: "right",
+        width: "84%",
+        paddingTop: "5px"
+
+   }
 
     return (
-        <div>
-            <h1>{school.name}</h1>
-            
+        <div style = {schoolStyle}>
+            <div style = {leftStyle}>
+
+                <img src = {image.default} style = {{display: "block", marginLeft: "auto", marginRight: "auto", maxHeight: school.imgSize}} alt = {school.name} />
+
+            </div>
+
+            <div style = {rightStyle}>
+
+                <h3>{school.name}</h3>
+
+                <h5>{school.descriptor1}</h5>
+                <h5>{school.descriptor2}</h5>
+
+            </div>
         </div>
     )
 }
