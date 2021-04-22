707b9c62f6f2043b9d96022eaa8521f401acf991
Siddharth Bharthulwar <47193327+sbharthulwar01@users.noreply.github.com>
2021-04-22 10:23:13 -0600

    add fully3d

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,6 +3,8 @@ import Pdf from '../../res/2021resume.pdf'
 
 const About = () => {
 
+    var image = require('../../img/casual.jpg')
+
     const style = {
 
         alignItems: "center",
@@ -24,20 +26,23 @@ const About = () => {
         fontSize : "16px",
         margin : "4px 2px",
         cursor: "pointer",
-        borderRadius: "8px"
+        borderRadius: "8px",
+        marginBottom : "20px"
     }
 
 
     return (
         <div style = {style}>
             <h2>About Me</h2>
-            <h4 style = {{color: "white", marginBottom : "25px"}}>Hi! My name is Siddharth (Sid) Bharthulwar, and I'm a student researcher and software engineer. </h4>
+            <div style = {{float: "right"}}>
+                <h4 style = {{color: "white", marginBottom : "25px"}}>Hi! My name is Siddharth (Sid) Bharthulwar, and I'm an incoming freshman at Harvard College. Previously, I conducted research at the University of Pennsylvania's Medical School and the University of Colorado Boulder.</h4>
 
-            <a href={Pdf} without rel="noopener noreferrer" target="_blank">
-                <button trailingIcon="picture_as_pdf" label="Resume" style = {buttonStyle}>
-                    Here's my Resume
-                </button>
-            </a>
+                <a href={Pdf} without rel="noopener noreferrer" target="_blank">
+                    <button trailingIcon="picture_as_pdf" label="Resume" style = {buttonStyle}>
+                        Here's my Resume
+                    </button>
+                </a>
+            </div>
         </div>
     )
 }
